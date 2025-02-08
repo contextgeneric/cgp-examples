192cd22577bcad96458c6b8fa1bcb8f451fabad0
Soares Chen <soares.chen@maybevoid.com>
2025-02-08 11:54:53 +0000

    Define concrete transfer request

--- transfer/src/types/requests/transfer.rs
+++ transfer/src/types/requests/transfer.rs
@@ -1,4 +1,8 @@
+use axum::extract::Query;
 use cgp::prelude::*;
+use headers::authorization::Basic;
+use headers::Authorization;
+use serde::Deserialize;
 
 use crate::types::DemoCurrency;
 
@@ -9,3 +13,24 @@ pub struct TransferRequest {
     pub quantity: u64,
     pub auth_header: Option<(String, String)>,
 }
+
+impl From<AxumTransferRequest> for TransferRequest {
+    fn from((Query(query), auth): AxumTransferRequest) -> Self {
+        Self {
+            currency: query.currency,
+            recipient: query.recipient,
+            quantity: query.quantity,
+            auth_header: auth
+                .map(|Authorization(basic)| (basic.username().into(), basic.password().into())),
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct TransferQuery {
+    pub currency: DemoCurrency,
+    pub recipient: String,
+    pub quantity: u64,
+}
+
+pub type AxumTransferRequest = (Query<TransferQuery>, Option<Authorization<Basic>>);
